7c452da92400a8afcd14a9e39a8172b925348661
LiziCYs <1832844180@qq.com>
2020-03-03 16:11:38 +0800

    init

---
+++ views/budget.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="l-budget">
+        {{name}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'l-budget',
+    data(){
+        return {
+            name: 'abc'
+        }
+    }
+}
+</script>
